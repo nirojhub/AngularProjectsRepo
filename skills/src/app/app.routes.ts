6ae66631ed8929fa53959fdfd5285abcd1352755
Niroj <nirojkumarsahoo55@gmail.com>
2024-04-09 22:00:17 +0530

    Added Service

--- skills/src/app/app.routes.ts
+++ skills/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from './register/register.component';
 import { EmployeesComponent } from './employees/employees.component';
+import { LoginComponent } from './login/login.component';
 export const routes: Routes = [
     {path:'', redirectTo: '/home', pathMatch: 'full'},
     {path:'home', component:HomeComponent},
     {path:'register', component:RegisterComponent},
-    {path:'employees', component:EmployeesComponent}
+    {path:'employees', component:EmployeesComponent},
+    {path:'login', component:LoginComponent}
 ];
---
+++ skills/src/app/login/login.component.ts
@@ -0,0 +1,71 @@
+import { CommonModule, formatCurrency } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators} from '@angular/forms'
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { AuthService } from "./../auth.service";
+import { Subscription } from 'rxjs';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, HttpClientModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit{
+  user$! : Observable<any>;
+  loginStatus$!:Observable<boolean>;
+  private lSubscription: Subscription = new Subscription;
+  frmLogin: FormGroup;
+  constructor(private http:HttpClient, private router:Router, 
+    private auth:AuthService)
+    {
+    this.frmLogin = this.createFormGroup();
+  }
+  ngOnInit(): void {  }
+  createFormGroup(){
+    return new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  onSubmit(): void {
+    let currentUser = this.frmLogin.value.username;
+    let currentPassword = this.frmLogin.value.password;
+    this.loginStatus$ = this.auth.login(this.frmLogin.value);
+    this.lSubscription = this.loginStatus$.subscribe(data=>{
+      if (data) {
+        console.log("user is Valid");
+        this.router.navigateByUrl('home');
+      }
+      else{
+        console.log("Invalid user");
+        this.router.navigateByUrl('login');
+      }
+    });
+
+    /* his. user$ = this.http.get(
+      "http://localhost:3000/employees",
+      {
+        params:{username:currentUser, password:currentPassword}
+      }
+    );
+    this.user$.subscribe(data => {
+      if (data.length > 0 && data[0].password == currentPassword) {
+        console.log("user is Valid");
+        localStorage.setItem('validUser', currentUser);
+        this.router.navigateByUrl('home');
+      }
+      else{
+        console.log("Invalid user");
+        this.router.navigateByUrl('login');
+      }
+    }); */
+  }
+  ngOnDestroy(): void {
+    this.lSubscription.unsubscribe();
+  }
+
+}
